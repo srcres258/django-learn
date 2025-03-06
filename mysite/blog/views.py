8ec9922bd0dcf7022e90c3ac6302e60d65b0b44f
src_resources <src.res.211@gmail.com>
2025-03-06 17:24:23 +0800

    Chapter 9.1

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from .models import Category, Post
+
 # Create your views here.
 
 def index(request):
     # 查询首页数据并显示在页面。
-    return render(request, 'blog/index.html')
+    category_list = Category.objects.all() # 查询到所有的分类
+    post_list = Post.objects.all() # 查询到所有的文章
+    context = { 'category_list': category_list, 'post_list': post_list }
+    return render(request, 'blog/index.html', context)
