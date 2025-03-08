dff8968bb56385153c220e79d7fe80f6497dc24f
src_resources <src.res.211@gmail.com>
2025-03-08 11:55:50 +0800

    Chapter 14: replace CKEditor to legacy version

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -12,43 +12,6 @@ for model in [Category, Tag, Sidebar]:
     admin.site.register(model)
 
 
-def absolute_path(path):
-    """
-    Given a relative or absolute path to a static asset, return an absolute
-    path. An absolute path will be returned unchanged while a relative path
-    will be passed to django.templatetags.static.static().
-    """
-    if path.startswith(("http://", "https://", "/")):
-        return path
-    return static(path)
-
-
-class ModuleJS(SafeData):
-    __slots__ = ('path',)
-
-    def __init__(self, path: str):
-        self.path = path
-
-    def __html__(self):
-        return format_html('<script type="module" src="{}"></script>', absolute_path(self.path))
-
-
-class ImportMapJS(SafeData):
-    __slots__ = ('import_map',)
-
-    def __init__(self, import_map: dict[str, str]):
-        self.import_map = import_map
-
-    def __html__(self):
-        script_content = '{"imports":{'
-        for k, v in self.import_map.items():
-            script_content += f'"{k}":"{absolute_path(v)}",'
-        script_content = script_content[:-1] # 去除尾随逗号，因为JSON标准中不允许出现尾随逗号
-        script_content += '}}'
-
-        return f'<script type="importmap">{script_content}</script>'
-
-
 class PostAdmin(admin.ModelAdmin):
     """文章详情管理"""
 
@@ -59,15 +22,14 @@ class PostAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
 
     class Media:
+        css = {
+            'all': ('ckeditor5/cked.css',)
+        }
         js = (
-            'https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.js',
-            ImportMapJS({
-                "ckeditor5": "ckeditor5/ckeditor5.js",
-                "ckeditor5/": "ckeditor5/"
-            }),
-            ModuleJS('ckeditor5/config.js'),
-            # 'ckeditor5/ckeditor5.js',
-            # 'ckeditor5/translations/zh.js'
+            'jquery.js',
+            'ckeditor5/ckeditor.js',
+            'ckeditor5/translations/zh.js',
+            'ckeditor5/config.js'
         )
 
 
--- mysite/blog/static/ckeditor5/config.js
+++ mysite/blog/static/ckeditor5/config.js
@@ -1,295 +1,78 @@
-import {
-    ClassicEditor,
-    Autoformat,
-    AutoImage,
-    Autosave,
-    BalloonToolbar,
-    BlockQuote,
-    BlockToolbar,
-    Bold,
-    CloudServices,
-    Emoji,
-    Essentials,
-    FindAndReplace,
-    FullPage,
-    GeneralHtmlSupport,
-    Heading,
-    HtmlComment,
-    HtmlEmbed,
-    ImageBlock,
-    ImageCaption,
-    ImageEditing,
-    ImageInline,
-    ImageInsert,
-    ImageInsertViaUrl,
-    ImageResize,
-    ImageStyle,
-    ImageTextAlternative,
-    ImageToolbar,
-    ImageUpload,
-    ImageUtils,
-    Indent,
-    IndentBlock,
-    Italic,
-    Link,
-    LinkImage,
-    List,
-    ListProperties,
-    Markdown,
-    MediaEmbed,
-    Mention,
-    PageBreak,
-    Paragraph,
-    PasteFromMarkdownExperimental,
-    PasteFromOffice,
-    ShowBlocks,
-    SimpleUploadAdapter,
-    SourceEditing,
-    SpecialCharacters,
-    SpecialCharactersArrows,
-    SpecialCharactersCurrency,
-    SpecialCharactersEssentials,
-    SpecialCharactersLatin,
-    SpecialCharactersMathematical,
-    SpecialCharactersText,
-    Table,
-    TableCaption,
-    TableCellProperties,
-    TableColumnResize,
-    TableProperties,
-    TableToolbar,
-    TextPartLanguage,
-    TextTransformation,
-    Title,
-    TodoList,
-    Underline,
-    WordCount
-} from 'ckeditor5';
-
-import translations from 'ckeditor5/translations/zh-cn.js';
-
-const LICENSE_KEY =
-    'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDI2MDE1OTksImp0aSI6IjM3MDgxYzU2LTJiMTEtNDdjYi05YzcxLWUzZTk0ZmZiZWUyYSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImJiOTU4NDEyIn0.1aVkR0lEaTSHQVwyW4p4j7GrGviHNnxwma5yoNWqjUkbj620kBAGrDmzSk6QmyLbMRHhA1mVgbfSuR2rM9hd3w';
-
-const editorConfig = {
-    toolbar: {
-        items: [
-            'sourceEditing',
-            'showBlocks',
-            'findAndReplace',
-            'textPartLanguage',
-            '|',
-            'heading',
-            '|',
-            'bold',
-            'italic',
-            'underline',
-            '|',
-            'emoji',
-            'specialCharacters',
-            'pageBreak',
-            'link',
-            'insertImage',
-            'insertImageViaUrl',
-            'mediaEmbed',
-            'insertTable',
-            'blockQuote',
-            'htmlEmbed',
-            '|',
-            'bulletedList',
-            'numberedList',
-            'todoList',
-            'outdent',
-            'indent'
-        ],
-        shouldNotGroupWhenFull: false
-    },
-    plugins: [
-        Autoformat,
-        AutoImage,
-        Autosave,
-        BalloonToolbar,
-        BlockQuote,
-        BlockToolbar,
-        Bold,
-        CloudServices,
-        Emoji,
-        Essentials,
-        FindAndReplace,
-        FullPage,
-        GeneralHtmlSupport,
-        Heading,
-        HtmlComment,
-        HtmlEmbed,
-        ImageBlock,
-        ImageCaption,
-        ImageEditing,
-        ImageInline,
-        ImageInsert,
-        ImageInsertViaUrl,
-        ImageResize,
-        ImageStyle,
-        ImageTextAlternative,
-        ImageToolbar,
-        ImageUpload,
-        ImageUtils,
-        Indent,
-        IndentBlock,
-        Italic,
-        Link,
-        LinkImage,
-        List,
-        ListProperties,
-        Markdown,
-        MediaEmbed,
-        Mention,
-        PageBreak,
-        Paragraph,
-        PasteFromMarkdownExperimental,
-        PasteFromOffice,
-        ShowBlocks,
-        SimpleUploadAdapter,
-        SourceEditing,
-        SpecialCharacters,
-        SpecialCharactersArrows,
-        SpecialCharactersCurrency,
-        SpecialCharactersEssentials,
-        SpecialCharactersLatin,
-        SpecialCharactersMathematical,
-        SpecialCharactersText,
-        Table,
-        TableCaption,
-        TableCellProperties,
-        TableColumnResize,
-        TableProperties,
-        TableToolbar,
-        TextPartLanguage,
-        TextTransformation,
-        Title,
-        TodoList,
-        Underline,
-        WordCount
-    ],
-    balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
-    blockToolbar: ['bold', 'italic', '|', 'link', 'insertImage', 'insertTable', '|', 'bulletedList', 'numberedList', 'outdent', 'indent'],
-    heading: {
-        options: [
-            {
-                model: 'paragraph',
-                title: 'Paragraph',
-                class: 'ck-heading_paragraph'
-            },
-            {
-                model: 'heading1',
-                view: 'h1',
-                title: 'Heading 1',
-                class: 'ck-heading_heading1'
-            },
-            {
-                model: 'heading2',
-                view: 'h2',
-                title: 'Heading 2',
-                class: 'ck-heading_heading2'
-            },
-            {
-                model: 'heading3',
-                view: 'h3',
-                title: 'Heading 3',
-                class: 'ck-heading_heading3'
-            },
-            {
-                model: 'heading4',
-                view: 'h4',
-                title: 'Heading 4',
-                class: 'ck-heading_heading4'
-            },
-            {
-                model: 'heading5',
-                view: 'h5',
-                title: 'Heading 5',
-                class: 'ck-heading_heading5'
-            },
-            {
-                model: 'heading6',
-                view: 'h6',
-                title: 'Heading 6',
-                class: 'ck-heading_heading6'
-            }
-        ]
-    },
-    htmlSupport: {
-        allow: [
-            {
-                name: /^.*$/,
-                styles: true,
-                attributes: true,
-                classes: true
-            }
-        ]
-    },
-    image: {
-        toolbar: [
-            'toggleImageCaption',
-            'imageTextAlternative',
-            '|',
-            'imageStyle:inline',
-            'imageStyle:wrapText',
-            'imageStyle:breakText',
-            '|',
-            'resizeImage'
-        ]
-    },
-    language: 'zh-cn',
-    licenseKey: 'GPL',
-    link: {
-        addTargetToExternalLinks: true,
-        defaultProtocol: 'https://',
-        decorators: {
-            toggleDownloadable: {
-                mode: 'manual',
-                label: 'Downloadable',
-                attributes: {
-                    download: 'file'
-                }
-            }
-        }
-    },
-    list: {
-        properties: {
-            styles: true,
-            startIndex: true,
-            reversed: true
-        }
-    },
-    mention: {
-        feeds: [
-            {
-                marker: '@',
-                feed: [
-                    /* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
-                ]
-            }
-        ]
-    },
-    menuBar: {
-        isVisible: true
-    },
-    placeholder: 'Type or paste your content here!',
-    table: {
-        contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
-    },
-    translations: [translations]
-};
-
 $(document).ready(function () {
-    ClassicEditor.create(document.querySelector('#id_content'), editorConfig).then(editor => {
-        // const wordCount = editor.plugins.get('WordCount');
-        // document.querySelector('#editor-word-count').appendChild(wordCount.wordCountContainer);
-        //
-        // document.querySelector('#editor-menu-bar').appendChild(editor.ui.view.menuBarView.element);
+    ClassicEditor
+        .create(document.querySelector('#id_content'), {
 
-        window.editor = editor;
+            toolbar: {
+                items: [
+                    'heading',
+                    '|',
+                    'blockQuote',
+                    'bold',
+                    'fontColor',
+                    'fontFamily',
+                    'fontSize',
+                    'italic',
+                    'highlight',
+                    'link',
+                    'bulletedList',
+                    'numberedList',
+                    '|',
+                    'outdent',
+                    'indent',
+                    '|',
+                    'imageUpload',
+                    'imageInsert',
+                    'insertTable',
+                    'mediaEmbed',
+                    'undo',
+                    'redo',
+                    'CKFinder',
+                    'code',
+                    'codeBlock',
+                    'fontBackgroundColor',
+                    'findAndReplace',
+                    'horizontalLine',
+                    'htmlEmbed',
+                    'todoList',
+                    'superscript',
+                    'subscript',
+                    'sourceEditing'
+                ]
+            },
+            language: 'zh-cn',
+            image: {
+                toolbar: [
+                    'imageTextAlternative',
+                    'imageStyle:inline',
+                    'imageStyle:block',
+                    'imageStyle:side'
+                ]
+            },
+            // 上传图片url配置
+            ckfinder: {
+                uploadUrl: '/uploads/'
+            },
+            table: {
+                contentToolbar: [
+                    'tableColumn',
+                    'tableRow',
+                    'mergeTableCells'
+                ]
+            },
+            licenseKey: '',
 
-        return editor;
-    });
-});
+        })
+        .then(editor => {
+            window.editor = editor;
+
+
+
+
+        })
+        .catch(error => {
+            console.error('Oops, something went wrong!');
+            console.error('Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:');
+            console.warn('Build id: l0rtbv6zc870-j0z9q43obeqd');
+            console.error(error);
+        });
+})
